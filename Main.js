8c0b2a1f773d3f8fb00fc109ed7453bd01ca1896
FilippaKD <fd222hz@student.lnu.se>
2025-03-24 12:06:07 +0100

    offline

--- Main.js
+++ Main.js
@@ -11,6 +11,17 @@ function Main() {
     new LowerMeny();
     new UpperMeny();
 
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/sw.js").then (
+            (registration) => {
+                console.log("Det gick");
+            },
+            (error) => {
+                console.log("Det gick inte");
+            },
+        );
+    }
+
 }
 window.addEventListener("load", function () {
     new Main();
